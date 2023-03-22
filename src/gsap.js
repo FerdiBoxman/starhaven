30b73ced5740d4f7306bec86a2ca054d418d4ab1
FerdiBoxman <95146698+FerdiBoxman@users.noreply.github.com>
2023-03-22 11:49:07 +0100

    Update gsap.js

--- src/gsap.js
+++ src/gsap.js
@@ -1,3 +1,5 @@
+//Animations Starhaven.ai
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>;
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollTrigger.min.js"></script>;
