c9087de6461d68408f25ec8357239f09c49d9d7b
FerdiBoxman <95146698+FerdiBoxman@users.noreply.github.com>
2023-04-06 22:21:25 +0200

    w

--- src/starhaven-gsapv1-92.jsx
+++ src/starhaven-gsapv1-92.jsx
@@ -1,339 +1,457 @@
-// //Animations Starhaven.ai0
+//Animations Starhaven.ai0
 
 
-// (function() {
-// gsap.registerPlugin(ScrollTrigger);
 
-// $('.section_key-visual').each(function () {
-//   let triggerElement = $(this);
-//   let targetElement = $('.key-visual_sticky-element');
+  gsap.registerPlugin(ScrollTrigger);
 
-//   let tl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: triggerElement,
+  $(".section_key-visual").each(function () {
+    let triggerElement = $(this);
+    let targetElement = $(".key-visual_sticky-element");
 
-//       start: 'top top',
-//       end: 'center center',
-//       scrub: 1,
-//     },
-//   });
-//   tl.fromTo(
-//     targetElement,
-//     {
-//       width: '60em',
-//       height: '40em',
-//     },
-//     {
-//       width: '100vw',
-//       height: '100vh',
-//     }
-//   );
-// });
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: triggerElement,
 
-// function setTrackHeights() {
-//   $('.alignment-cycle_height').each(function () {
-//     let trackWidth = $(this).find('.track').outerWidth();
-//     $(this).height(trackWidth);
-//   });
-// }
-// setTrackHeights();
+        start: "top top",
+        end: "center center",
+        scrub: 1,
+      },
+    });
+    tl.fromTo(
+      targetElement,
+      {
+        width: "60em",
+        height: "40em",
+      },
+      {
+        width: "100vw",
+        height: "100vh",
+      }
+    );
+  });
 
-// window.addEventListener('resize', function () {
-//   setTrackHeights();
-// });
+  function setTrackHeights() {
+    $(".alignment-cycle_height").each(function () {
+      let trackWidth = $(this).find(".track").outerWidth();
+      $(this).height(trackWidth);
+    });
+  }
+  setTrackHeights();
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   let tlCounter = gsap.timeline({ delay: 1 });
+  window.addEventListener("resize", function () {
+    setTrackHeights();
+  });
 
-//   tlCounter.to('.loader_progress-bar', {
-//     width: '100%',
-//     duration: 4,
-//     // voeg een kleine vertraging toe van 0.5 seconden
-//     ease: CustomEase.create(
-//       'custom',
-//       'M0,0 C0,0.037 0.182,0.09 0.204,0.12 0.266,0.204 0.264,0.244 0.3,0.3 0.386,0.433 0.563,0.498 0.598,0.532 0.611,0.545 0.669,0.737 0.71,0.77 0.82,0.858 0.696,1 1,1  '
-//     ),
-//     onUpdate: function () {
-//       const progress = (tlCounter.progress() * 100).toFixed(2);
-//       const countdown = (4 - tlCounter.progress() * 4).toFixed(2);
-//       document.querySelector('.loader_progress-bar').style.width = `${progress}%`;
-//       document.querySelector('.preloader_countdown').textContent = countdown;
-//     },
-//   });
-// });
+  document.addEventListener("DOMContentLoaded", function () {
+    let tlCounter = gsap.timeline({ delay: 1 });
 
-// const navLinks = document.querySelectorAll("[nav-link='scramble']");
+    tlCounter.to(".loader_progress-bar", {
+      width: "100%",
+      duration: 4,
+      // voeg een kleine vertraging toe van 0.5 seconden
+      ease: CustomEase.create(
+        "custom",
+        "M0,0 C0,0.037 0.182,0.09 0.204,0.12 0.266,0.204 0.264,0.244 0.3,0.3 0.386,0.433 0.563,0.498 0.598,0.532 0.611,0.545 0.669,0.737 0.71,0.77 0.82,0.858 0.696,1 1,1  "
+      ),
+      onUpdate: function () {
+        const progress = (tlCounter.progress() * 100).toFixed(2);
+        const countdown = (4 - tlCounter.progress() * 4).toFixed(2);
+        document.querySelector(".loader_progress-bar").style.width = `${progress}%`;
+        document.querySelector(".preloader_countdown").textContent = countdown;
+      },
+    });
+  });
 
-// navLinks.forEach((link) => {
-//   const text = link.textContent;
+  const navLinks = document.querySelectorAll("[nav-link='scramble']");
 
-//   link.style.display = 'inline-flex'; // of 'inline-block'
-//   link.style.width = `${link.offsetWidth}px`;
+  navLinks.forEach((link) => {
+    const text = link.textContent;
 
-//   link.addEventListener('mouseenter', () => {
-//     gsap.to(link, {
-//       scrambleText: {
-//         chars: text,
-//         text: text,
-//         speed: 1,
-//         delimiter: '',
-//         revealDelay: 0.5,
-//       },
-//       duration: 0.5,
-//     });
-//   });
-// });
+    link.style.display = "inline-flex"; // of "inline-block"
+    link.style.width = `${link.offsetWidth}px`;
 
-// // section_header
+    link.addEventListener("mouseenter", () => {
+      gsap.to(link, {
+        scrambleText: {
+          chars: text,
+          text: text,
+          speed: 1,
+          delimiter: "",
+          revealDelay: 0.5,
+        },
+        duration: 0.5,
+      });
+    });
+  });
 
-// $('.section_header').each(function () {
-//   let triggerElement = $(this);
-//   let targetElement = $('.header_background-overlay');
-//   let textElements = $("[header-text-animation='opacity']");
 
-//   let tl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: triggerElement,
-//       start: 'top center',
-//       end: 'bottom-=10%',
-//       scrub: 1,
-//     },
-//   });
 
-//   let tl2 = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: triggerElement,
-//       start: 'top center',
-//       end: 'bottom-=20%',
-//       scrub: 1,
-//     },
-//   });
+// section_header
 
-//   tl.to(textElements, {
-//     delay: 1.5,
-//     opacity: 0,
-//     duration: 1,
-//   });
+$(".section_header").each(function () {
+  let triggerElement = $(this);
+  let targetElement = $(".header_background-overlay");
+  let textElements = $("[header-text-animation='opacity']");
 
-//   tl2.to(targetElement, {
-//     backgroundColor: 'rgba(0, 0, 0, 1)',
-//     ease: 'power1.inOut',
-//     duration: 1,
-//   });
-// });
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "top center",
+      end: "bottom-=10%",
+      scrub: 1,
+    },
+  });
 
-// //Section_intro
+  let tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "top center",
+      end: "bottom-=20%",
+      scrub: 1,
+    },
+  });
 
-// $('.section_intro').each(function () {
-//   let triggerElement = $(this);
-//   let targetElements = $('.intro_left-grid, .intro_grid-top-right-first, .intro_right-grid-bottom');
+  tl.to(textElements, {
+    delay: 1.5,
+    opacity: 0,
+    duration: 1,
+  });
 
-//   let introTextElements = $("[text-animation='opacity']", this);
+  tl2.to(targetElement, {
+    backgroundColor: "rgba(0, 0, 0, 1)",
+    ease: "power1.inOut",
+    duration: 1,
+  });
+});
 
-//   let introImageElement = $('.intro_grid-image', this);
+//Section_intro
 
-//   let tl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: triggerElement,
-//       start: 'top top',
-//       end: 'top+=5%',
-//       scrub: 1,
-//     },
-//   });
+$(".section_intro").each(function () {
+  let triggerElement = $(this);
+  let targetElements = $(".intro_left-grid, .intro_grid-top-right-first, .intro_right-grid-bottom");
 
-//   tl.fromTo(
-//     targetElements,
-//     {
-//       'border-right-color': 'black',
-//       'border-top-color': 'black',
-//     },
-//     {
-//       'border-right-color': '#302e2e',
-//       'border-top-color': '#302e2e',
-//       duration: 1,
-//       ease: 'power2.inOut',
-//     }
-//   );
+  let introTextElements = $("[text-animation='opacity']", this);
 
-//   tl.fromTo(
-//     introTextElements,
-//     {
-//       opacity: 0,
-//     },
-//     {
-//       opacity: 1,
-//       duration: 1,
-//       ease: 'power2.inOut',
-//     },
-//     1
-//   );
+  let introImageElement = $(".intro_grid-image", this);
 
-//   let introImageTl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: triggerElement,
-//       start: 'top top',
-//       end: 'top+=5%',
-//       scrub: 1,
-//     },
-//   });
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "top top",
+      end: "top+=5%",
+      scrub: 1,
+    },
+  });
 
-//   introImageTl.fromTo(
-//     introImageElement,
-//     {
-//       opacity: 0,
-//     },
-//     {
-//       opacity: 1,
-//       duration: 1,
-//       ease: 'power2.inOut',
-//     }
-//   );
+  tl.fromTo(
+    targetElements,
+    {
+      "border-right-color": "black",
+      "border-top-color": "black",
+    },
+    {
+      "border-right-color": "#302e2e",
+      "border-top-color": "#302e2e",
+      duration: 1,
+      ease: "power2.inOut",
+    }
+  );
 
-//   let reverseTl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: triggerElement,
-//       start: 'center+=5%',
-//       end: 'bottom bottom',
-//       scrub: 1,
-//       ease: 'power2.inOut',
-//       reverse: true,
-//     },
-//   });
+  tl.fromTo(
+    introTextElements,
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 1,
+      ease: "power2.inOut",
+    },
+    1
+  );
 
-//   reverseTl.to(
-//     targetElements,
-//     {
-//       'border-right-color': 'black',
-//       'border-top-color': 'black',
-//       duration: 1,
-//     },
-//     0.05
-//   );
+  let introImageTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "top top",
+      end: "top+=5%",
+      scrub: 1,
+    },
+  });
 
-//   reverseTl.to(
-//     introTextElements,
-//     {
-//       opacity: 0,
-//       duration: 1,
-//     },
-//     0.05
-//   );
+  introImageTl.fromTo(
+    introImageElement,
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 1,
+      ease: "power2.inOut",
+    }
+  );
 
-//   reverseTl.to(
-//     introImageElement,
-//     {
-//       opacity: 0,
-//       duration: 1,
-//     },
-//     0.05
-//   );
-// });
+  let reverseTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "center+=5%",
+      end: "bottom bottom",
+      scrub: 1,
+      ease: "power2.inOut",
+      reverse: true,
+    },
+  });
+});
+$(".section_header").each(function () {
+  let triggerElement = $(this);
+  let targetElement = $(".header_background-overlay");
+  let textElements = $("[header-text-animation='opacity']");
 
-// //Planet manifesto animation
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "top center",
+      end: "bottom-=10%",
+      scrub: 1,
+    },
+  });
 
-// gsap.registerPlugin(ScrollTrigger);
+  let tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "top center",
+      end: "bottom-=20%",
+      scrub: 1,
+    },
+  });
 
-// let triggerElement = $('.section_manifesto');
-// let fsglobeElement = $('.fs-globe-container');
-// let textElements = $("[text-animation='opacity']");
+  tl.to(textElements, {
+    delay: 1.5,
+    opacity: 0,
+    duration: 1,
+  });
 
-// let tl = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: triggerElement,
-//     start: 'top top-=5%',
-//     end: 'center center',
-//     scrub: 1,
-//   },
-// });
+  tl2.to(targetElement, {
+    backgroundColor: "rgba(0, 0, 0, 1)",
+    ease: "power1.inOut",
+    duration: 1,
+  });
+});
 
-// tl.fromTo(
-//   fsglobeElement,
-//   {
-//     x: '-50%',
-//   },
-//   {
-//     x: '0%',
-//     duration: 2,
-//     ease: 'power2.out',
-//   }
-// );
+//Section_intro
 
-// tl.fromTo(
-//   textElements,
-//   {
-//     opacity: 0,
-//     y: 30,
-//   },
-//   {
-//     opacity: 1,
-//     y: 0,
-//     duration: 1,
-//     ease: 'power2.inOut',
-//   },
-//   '-=0.5'
-// );
+$(".section_intro").each(function () {
+  let triggerElement = $(this);
+  let targetElements = $(".intro_left-grid, .intro_grid-top-right-first, .intro_right-grid-bottom");
 
-// tl.fromTo(
-//   '.manifesto_left-grid-wrapper',
-//   {
-//     'border-right-color': 'black',
-//   },
-//   {
-//     'border-right-color': '#302e2e',
-//     duration: 1,
-//     ease: 'power2.inOut',
-//   },
-// );
+  let introTextElements = $("[text-animation='opacity']", this);
 
-// let reverseTl = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: triggerElement,
-//     start: 'center+=5%',
-//     end: 'bottom bottom',
-//     scrub: 1,
-//     ease: 'power2.inOut',
-//     toggleActions: 'play none reverse none',
-//   },
-// });
+  let introImageElement = $(".intro_grid-image", this);
 
-// reverseTl.to(
-//   '.manifesto_left-grid-wrapper',
-//   {
-//     'border-right-color': 'black',
-//     duration: 1,
-//   },
-//   0
-// );
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "top top",
+      end: "top+=5%",
+      scrub: 1,
+    },
+  });
 
-// reverseTl.to(
-//   textElements,
-//   {
-//     opacity: 0,
-//     duration: 1,
-//   },
-//   0
-// );
+  tl.fromTo(
+    targetElements,
+    {
+      "border-right-color": "black",
+      "border-top-color": "black",
+    },
+    {
+      "border-right-color": "#302e2e",
+      "border-top-color": "#302e2e",
+      duration: 1,
+      ease: "power2.inOut",
+    }
+  );
 
-// reverseTl.fromTo(
-//   '.manifesto_left-grid-wrapper',
-//   {
-//     duration: 2,
-//     ease: 'power2.out',
-//   },
-//   0
-// );
+  tl.fromTo(
+    introTextElements,
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 1,
+      ease: "power2.inOut",
+    },
+    1
+  );
 
-// reverseTl.to(
-//   '.fs-globe-container',
-//   {
-//     x: '-50%',
-//     duration: 2,
-//     ease: 'power2.inOut',
-//   },
-//   0.05
-// );
+  let introImageTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "top top",
+      end: "top+=5%",
+      scrub: 1,
+    },
+  });
 
-// })();
+  introImageTl.fromTo(
+    introImageElement,
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 1,
+      ease: "power2.inOut",
+    }
+  );
+
+  let reverseTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "center+=5%",
+      end: "bottom bottom",
+      scrub: 1,
+      ease: "power2.inOut",
+      reverse: true,
+    },
+  });
+});
+
+
+  reverseTl.to(
+    targetElements,
+    {
+      "border-right-color": "black",
+      "border-top-color": "black",
+      duration: 1,
+    },
+    0.05
+  );
+
+  reverseTl.to(
+    introTextElements,
+    {
+      opacity: 0,
+      duration: 1,
+    },
+    0.05
+  );
+
+  reverseTl.to(
+    introImageElement,
+    {
+      opacity: 0,
+      duration: 1,
+    },
+    0.05
+  );
+
+
+//Planet manifesto animation
+
+gsap.registerPlugin(ScrollTrigger);
+
+let triggerElement = $(".section_manifesto");
+let fsglobeElement = $(".fs-globe-container");
+let textElements = $("[text-animation='opacity']");
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: triggerElement,
+    start: "top top-=5%",
+    end: "center center",
+    scrub: 1,
+  },
+});
+
+tl.fromTo(
+  fsglobeElement,
+  {
+    x: "-50%",
+  },
+  {
+    x: "0%",
+    duration: 2,
+    ease: "power2.out",
+  }
+);
+
+tl.fromTo(
+  textElements,
+  {
+    opacity: 0,
+    y: 30,
+  },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    ease: "power2.inOut",
+  },
+  "-=0.5"
+);
+
+tl.fromTo(
+  ".manifesto_left-grid-wrapper",
+  {
+    "border-right-color": "black",
+  },
+  {
+    "border-right-color": "#302e2e",
+    duration: 1,
+    ease: "power2.inOut",
+  },
+);
+
+let reverseTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: triggerElement,
+    start: "center+=5%",
+    end: "bottom bottom",
+    scrub: 1,
+    ease: "power2.inOut",
+    toggleActions: "play none reverse none",
+  },
+});
+
+reverseTl.to(
+  ".manifesto_left-grid-wrapper",
+  {
+    "border-right-color": "black",
+    duration: 1,
+  },
+  0
+);
+
+reverseTl.to(
+  textElements,
+  {
+    opacity: 0,
+    duration: 1,
+  },
+  0
+);
+
+reverseTl.fromTo(
+  ".manifesto_left-grid-wrapper",
+  {
+    duration: 2,
+    ease: "power2.out",
+  },
+  0
+);
+
+reverseTl.to(
+  ".fs-globe-container",
+  {
+    x: '-50%',
+    duration: 2,
+    ease: "power2.inOut",
+  },
+  0.05
+);
+
+
 
 //works from here
 
