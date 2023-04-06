d6908cd6c1e801f09fcd52421017bff950624a81
FerdiBoxman <95146698+FerdiBoxman@users.noreply.github.com>
2023-04-06 21:33:42 +0200

    e

--- bin/build.js
+++ bin/build.js
@@ -7,7 +7,7 @@ const BUILD_DIRECTORY = 'dist';
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 // Config entrypoint files
-const ENTRY_POINTS = ['src/index.ts', 'src/starhaven-gsapv1-8.jsx'];
+const ENTRY_POINTS = ['src/index.ts', 'src/starhaven-gsapv1-9.jsx'];
 
 // Config dev serving
 const LIVE_RELOAD = !PRODUCTION;
